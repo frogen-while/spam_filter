bbbfe46c15b604ac65fe86414c7b07fe864408cf
Daster <maksimkocubajlo@gmail.com>
2025-04-22 16:41:19 +0200

    胡說八道

--- __main__.py
+++ __main__.py
@@ -1,9 +1,12 @@
-
 import re
 import json
-def formatting(word):
+import math
+def formatting(word, urls):
+    urls+=1
     if "http" in word or "https" in word or "www" in word:
+        urls+=1
         return word.replace("http://", "").replace("https://", "").replace("www.", "").split('/')[0]
+        
     word = word.lower()
     word = word.strip('\'"')
     word = re.sub(r'[()\?\!\:\.\>\<\-\'\\d]', '', word)
@@ -20,37 +23,61 @@ def find_spam(massage):
         with open('spammers_domains.json', 'r') as f:
             spammers_domains = json.loads(f.read())
 
-        spam_chances = []
+        urls = 0
+        spamicities = []
         for word in massage.split():
-            word = formatting(word)
-            if word in spammers_domains:
-                return True
             try:
-                spamicity = bad_words.get(word) / (bad_words.get(word) + good_words.get(word))
-                spam_chances.append(spamicity)
-            except:
+                word = formatting(word, urls)
+                if word in spammers_domains:
+                    return True
+                try:
+                    bad_words_chances = bad_words.get(word, 0.001)
+                    good_words_chances = good_words.get(word, 0.001)
+                    spamicity = math.log(bad_words_chances / (good_words_chances + bad_words_chances))
+                    spamicities.append(spamicity)
+                except KeyError:
+                    continue
+                    
+            except KeyError as e:
+                print(f"KeyError: {e}")
                 continue
-        from math import log, exp
+        prd_spamicity_bad = 1
+        prd_spamicity_good = 1
+        for spamicity in spamicities:
+            if spamicity == 0:
+                continue
+            if spamicity < 0:
+                continue
+            prd_spamicity_bad *= spamicity
+        for spamicity in spamicities:
+            if spamicity == 0:
+                continue
+            if spamicity > 0:
+                continue
+            prd_spamicity_good *= (1-spamicity)
+        
+        score = (prd_spamicity_bad / (prd_spamicity_bad + prd_spamicity_good)) 
 
-        log_spam_prd = 0
-        log_spam_prd2 = 0
-        for item in spam_chances:
-            log_spam_prd += log(item)
-            log_spam_prd2 += log(1 - item)
-        score = exp(log_spam_prd) / (exp(log_spam_prd) + exp(log_spam_prd2))
-        print(
-            f"spam score: {score}")
-        if score > 0.8:
-            return True
+        if urls > 0:
+            score += 0.5
         else:
-            return False
-    except Exception as e:
-        print(e)
-        return
+            score = 0
+        
+        return score
+    except FileNotFoundError as e:
+        print(f"File not found: {e}")
+        return False
+    except json.JSONDecodeError as e:
+        print(f"JSON decode error: {e}")
+        return False
+                
 
 
-for i in range(1, 7):
-    if find_spam(f"msg{i}.txt"):
-        print("It is spam")
-    else:
-        print("It is not spam")
+if __name__ == "__main__":
+    for i in range(1, 7):
+        if find_spam(f"msg{i}.txt"):
+            print(find_spam(f"msg{i}.txt"))
+            print("It is spam")
+        else:
+            print(find_spam(f"msg{i}.txt"))
+            print("It is not spam")
